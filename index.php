26ea4d20518007908dca6e43185fc3c5507d3709
HarshitCoder234 <87566674+HarshitCoder234@users.noreply.github.com>
2021-08-19 21:19:02 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,49 +1,60 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<title>Calculator</title>
-</head>
-<body>
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Calculator</title>
+    </head>
+    <body>
+        <div>
+            <h1>Calculator</h1>
+        </div>
+        
+        <p>
+            <form method="get" action="index.php">
+                <input type="text"  name="n1">
+                <select name="action">
+                    <option value="plus">+</option>
+                    <option value="minus">-</option>
+                    <option value="star">*</option>
+                    <option value="slash">/</option>
+                </select>
+                <input type="text" name="n2">
+                <input type="submit" name="submit" value="Submit">
+            </form>
+        </p>
 
- <div>
- 	<h1>Calculator</h1>
- </div>
+        <p>
+            <?php
+                if(isset($_GET["submit"])) {
+                    $n1 = $_GET["n1"];
+                    $n2 = $_GET["n2"];
+                    $action = $_GET["action"];
 
-<p>
- <form method="get" action="index.php">
- 	<input name="v1">
- 	<select name="action">
- 		<option value="plus">+</option>
- 		<option value="minus">-</option>
- 	</select>
- 	<input name="v2">
- 	<input type="submit" name="submit" value="Submit">
- </form>
-</p>
+                    if ($action == "plus") {
+                        $result = $n1 + $n2;
+                        echo "<h1>$n1 + $n2 =";
+                    }
+                    elseif ($action == "minus") {
+                        $result = $n1 - $n2;
+                        echo "<h1>$n1 - $n2 =";
+                    }
+                    elseif ($action == "star") {
+                        $result = $n1 * $n2;
+                        echo "<h1>$n1 * $n2 =";
+                    }
+                    elseif($action == "slash" and $n2 == 0) {
+                        $result = "Zero Division Error : Divisor should not be Zero";
+                        echo "<h1>";
+                    }
+                    else {
+                        $result = $n1 / $n2;
+                        echo "<h1>$n1 / $n2 =";
+                    }
 
-<p>
- <?php 
-
- 	if(isset($_GET["submit"])) {
- 		$v1 = $_GET["v1"];
- 		$v2 = $_GET["v2"];
-
- 		if ($_GET["action"] == "plus") {
- 			$result = $v1 + $v2;
- 			echo "<h1>$v1 + $v2";
- 		}
- 		else {
- 			$result = $v1 - $v2;
- 			echo "<h1>$v1 - $v2";
- 		}
-
- 		echo " = $result</h1>";
- 	}
-
- ?>
-</p>
-
-</body>
+                    echo "$result</h1>";
+                }
+            ?>
+        </p>
+    </body>
 </html>
